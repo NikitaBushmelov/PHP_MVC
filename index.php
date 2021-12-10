1b96d95625cf48c6636eb8ab7947e5f8451c1fa6
NikitaBushmelov <bushmelovnikita@gmail.com>
2021-12-10 09:48:10 +0100

    master

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include "Views/header.php";
+?>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      
+    </div>
+  </div>
+</div>
+<?php
+include "Views/footer.php";
+?>
